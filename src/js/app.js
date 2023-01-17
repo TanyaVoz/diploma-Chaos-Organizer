c5b091c93e7734ff1bf3a06a06580b8ca0f56551
Tanya <voznyuk1998@mail.ru>
2023-01-18 00:28:05 +0300

    5

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import AppController from './appController';
 
-const manager = new AppController('wss://ahj-diploma-backend.vercel.app');
+const manager = new AppController('wss://ahj-diploma-backend.vercel.app/');
 manager.init();
